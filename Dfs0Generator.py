8f337d6d408da6d59d413e4e32fe3a8eb0054530
Piotr <38719982+Daldek@users.noreply.github.com>
2020-07-16 16:27:23 +0200

    Add files via upload
    Update

--- Dfs0Generator.py
+++ Dfs0Generator.py
@@ -1,12 +1,17 @@
 from datetime import datetime, timedelta
-from mikeio.dfs0 import dfs0
+from mikeio.dfs0 import Dfs0
 import numpy as np
 
 data = []
+data_value_type = [0, 1]
 d = np.random.random([20])
 data.append(d)
-dfs = dfs0()
+dfs = Dfs0()
 dfs.create(filename='RandomValues.dfs0',
            data=data,
            start_time=datetime(2020, 1, 1),
-           dt=60)
+           dt=60,
+           variable_type=[100000],
+           unit=[1000],
+           data_value_type=[0]
+           )
