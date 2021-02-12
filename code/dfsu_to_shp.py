4790244f7608aeaabc84e8492955cdd65c8dc641
Daldek <piotr.de.bever@gmail.com>
2021-02-12 14:40:21 +0100

    Dfsu_to_shp update (prefix)

--- code/dfsu_to_shp.py
+++ code/dfsu_to_shp.py
@@ -3,7 +3,6 @@ from functions import *
 input_file = input('Input Dfsu file: ')  # an absolute path
 item = input('Item: ')
 output_folder = input('Output folder: ')  # an absolute path
-prefix = input('The prefix to be added to the output file name: ')
 time_step = input('Time step: ')  # integer, counted from 0
 
 dfsu_to_shp(input_file, item, output_folder, int(time_step))
