59864c3e60d820989a6b6a0bc681640970739202
Daldek <piotr.de.bever@gmail.com>
2024-01-02 14:57:33 +0100

    Multi-file conversion

--- code/dfs2_to_ascii.py
+++ code/dfs2_to_ascii.py
@@ -1,7 +1,17 @@
+import os
 from classes import *
 
-in_dfs2 = input("Dfs2 file path: ")
-in_dfs2_path = in_dfs2.replace('"','')
 
-new_dfs2 = MyASCII(in_dfs2_path)
-new_dfs2.write_asc()
+path = input("Insert path to a dfs2 file or to a folder which containt such file(s): ")
+path = path.replace('"', '')
+
+if os.path.isfile(path):
+    new_dfs2 = MyASCII(path)
+    new_dfs2.write_asc()
+    print(path)
+else:
+    for f in os.listdir(path):
+        if f.endswith('.dfs2'):
+            new_dfs2 = MyASCII(os.path.join(path, f))
+            new_dfs2.write_asc()
+            print(f)
