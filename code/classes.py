a1fcd8eb8b80822f685e8cf5e6387dcdceefd22a
PL1B5J <1b5j@sweco.dk>
2024-10-10 12:12:08 +0200

    Update

--- code/classes.py
+++ code/classes.py
@@ -1,15 +1,14 @@
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import mikeio
 from mikeio import Dfs2
-from mikecore.eum import eumUnit
-from mikeio.eum import EUMType, ItemInfo
 
 
 class AscFile:
-    """This class analyzes an ASCII file header. This analysis can be used to compare 2
-    (or more) ASCII files used in Mike21 models or to create a numpy array to create
-    a new Dfs2 file"""
+    """Class for analyzing and manipulating ASCII grid files used in Mike21 models.
+    This class can compare generate random ASCII grids or create a numpy array for new Dfs2.
+    """
 
     def __init__(
         self,
@@ -25,92 +24,123 @@ class AscFile:
         """Class initialization
         :param path: path to an ASCII (ESRI grid) file"""
         self.path = path
-        self.ncols = ncols
-        self.nrows = nrows
-        self.xllcorner = xllcorner
-        self.yllcorner = yllcorner
-        self.cellsize = cellsize
-        self.nodata_value = nodata_value
-        self.data = data
+        self._ncols = ncols
+        self._nrows = nrows
+        self._xllcorner = xllcorner
+        self._yllcorner = yllcorner
+        self._cellsize = cellsize
+        self._nodata_value = nodata_value
+        self._data = data
+
+    def _validate_file(self):
+        "Validate if the input file exists and if it's an asc file"
+        f_path = Path(self.path)
+        if not f_path.exists():
+            raise FileNotFoundError(f"{self.path} does not exists!")
+        if f_path.suffix != ".asc":
+            raise ValueError(f"{self.path} in not an asc file!")
 
     def get_properties(self):
-        """Getting data from ASCII file and extracting raw numbers"""
+        """Retrieve properties from the ASCII file if not already set."""
+        self._validate_file()
+        if all(
+            attr is None
+            for attr in [
+                self._ncols,
+                self._nrows,
+                self._xllcorner,
+                self._yllcorner,
+                self._cellsize,
+                self._nodata_value,
+            ]
+        ):
+            with open(self.path, "r", encoding="utf-8") as f:
+                raw_header = [
+                    next(f).strip() for _ in range(6)
+                ]  # Use strip() to remove newlines
+                values = [
+                    float(item.split()[-1]) for item in raw_header
+                ]  # Extract values directly
 
-        with open(self.path, "r") as f:
-            raw_header = [next(f) for x in range(6)]
-            self.header = [
-                x[:-1] for x in raw_header
-            ]  # removing '\n' for the end of each line
-            values = [
-                float(list(item.split(" "))[-1]) for item in self.header
-            ]  # extraction of values
-
-        (
-            self.ncols,
-            self.nrows,
-            self.xllcorner,
-            self.yllcorner,
-            self.cellsize,
-            self.nodata_value,
-        ) = values
-        return values  # should I return properties as a list?
+            (
+                self._ncols,
+                self._nrows,
+                self._xllcorner,
+                self._yllcorner,
+                self._cellsize,
+                self._nodata_value,
+            ) = values
+        return (
+            self._ncols,
+            self._nrows,
+            self._xllcorner,
+            self._yllcorner,
+            self._cellsize,
+            self._nodata_value,
+        )
 
     def get_data(self):
-        """The get_data function returns a 1D list"""
-        with open(self.path) as f:
-            raw_data = f.readlines()[6:]
-            flattened_data = [float(x) for line in raw_data for x in line.split() if x]
+        """Returns a flattened 1D list of data from the ASCII file."""
+        if self._data is None:
+            with open(self.path, encoding="utf-8") as f:
+                raw_data = f.readlines()[6:]
+                flattened_data = [
+                    float(x) for line in raw_data for x in line.split() if x
+                ]
 
-        self.data = flattened_data
-        return self.data
+            self._data = flattened_data
+        return self._data
 
-    def get_data_as_3D_numpy_array(self):
-        """A 3D array was used in Mikeio's example. Two dimensions relate to X and Y coordinates,
-        while the third dimension relates to time and - it seems to me - is required
-        to specify at least one time step when creating a Dfs2 file."""
-        self.get_data()
-        self.array = np.array(self.data).reshape(1, int(self.nrows), int(self.ncols))
-        self.array[self.array == self.nodata_value] = "nan"
-        return self.array
+    def get_data_as_3d_numpy_array(self):
+        """Converts the data into a 3D numpy array suitable for Dfs2 files.
+        The array dimensions correspond to time, rows, and columns."""
+        data = np.array(self.get_data())
+        reshaped_data = data.reshape((1, int(self._nrows), int(self._ncols)))
+        reshaped_data[reshaped_data == self._nodata_value] = np.nan
+        return reshaped_data
 
     @staticmethod
     def generate_random_grid(nrows=5, ncols=5, no_data_value=-9999):
+        """Generates random 5x5 grid and saves it as an ASCII grid file"""
         data = np.random.randint(0, 100, size=(nrows, ncols))
 
-        with open("random_grid.asc", "w") as f:
+        with open("random_grid.asc", "w", encoding="utf-8") as f:
             f.write(f"NCOLS {ncols}\n")
             f.write(f"NROWS {nrows}\n")
-            f.write(f"XLLCORNER 0.0\n")
-            f.write(f"YLLCORNER 0.0\n")
-            f.write(f"CELLSIZE 1.0\n")
+            f.write("XLLCORNER 0.0\n")
+            f.write("YLLCORNER 0.0\n")
+            f.write("CELLSIZE 1.0\n")
             f.write(f"NODATA_VALUE {no_data_value}\n")
             np.savetxt(f, data, fmt="%d")
-
         return 1
 
 
 class MyDfs2(AscFile):
-    """Creating Dfs2 file based on an ascii file"""
+    """Class for creating a Dfs2 file based on an ASCII input file."""
 
-    def __init__(self, path, projection, item):
+    def __init__(self, path, projection, item=None):
+        """Initializes the MyDfs2 class.
+        :param path: Path to the ASCII file.
+        :param projection: Coordinate system projection for the grid.
+        :param item: Item information for the Dfs2 file. NOT YET IMPLEMENTED!"""
         super().__init__(path)
         self.projection = projection
         self.item = item
 
     def create_dfs2(self):
+        """Creates a Dfs2 file using properties from the ASCII file."""
         self.get_properties()
-        self.get_data_as_3D_numpy_array()
         grid = mikeio.Grid2D(
-            x0=self.xllcorner,
-            dx=self.cellsize,
-            y0=self.yllcorner,
-            dy=self.cellsize,
-            nx=int(self.ncols),
-            ny=int(self.nrows),
+            x0=self._xllcorner,
+            dx=self._cellsize,
+            y0=self._yllcorner,
+            dy=self._cellsize,
+            nx=int(self._ncols),
+            ny=int(self._nrows),
             projection=self.projection,
         )
         da = mikeio.DataArray(
-            data=np.flip(self.array, 1),
+            data=np.flip(self.get_data_as_3d_numpy_array(), 1),
             geometry=grid,
             time=pd.date_range("2100", periods=1, freq="D"),
             item=mikeio.ItemInfo("Example", mikeio.EUMType.Elevation),
@@ -119,42 +149,59 @@ class MyDfs2(AscFile):
         ds = mikeio.Dataset([da])
         dfs2_name = self.path[:-3] + "dfs2"
         ds.to_dfs(dfs2_name)
+        return dfs2_name
 
 
 class MyASCII:
-    """Creating ASCII file based on Dfs2"""
+    """Class for creating an ASCII grid from a Dfs2 input file."""
 
     def __init__(self, path):
+        """Initializes the MyASCII class.
+        :param path: Path to the Dfs2 file."""
         self.path = path
+        self._dfs2 = None
+        self._ncols = None
+        self._nrows = None
+        self._xllcorner = None
+        self._yllcorner = None
+        self._cellsize = None
+        self._nodata_value = None
+
+    def _validate_file(self):
+        "Validate if the input file exists and if it's an asc file"
+        f_path = Path(self.path)
+        if not f_path.exists():
+            raise FileNotFoundError(f"{self.path} does not exists!")
+        if f_path.suffix != ".dfs2":
+            raise ValueError(f"{self.path} in not a dfs2 file!")
 
     def get_properties(self):
-        self.dfs2 = Dfs2(self.path)
-        # self.ds = self.dfs2.read()[0]  # currently desiged to only read simple dfs2 files
-        # self.da = mikeio.dataset.Dataset(self.ds)
-        self.ncols = self.dfs2.nx
-        self.nrows = self.dfs2.ny
-        self.xllcorner = self.dfs2.origin[0] - (self.dfs2.dx / 2)
-        self.yllcorner = self.dfs2.origin[1] - (self.dfs2.dy / 2)
-        self.cellsize = self.dfs2.dx  # hope this value is same as dy
-        self.nodata_value = -9999  # default value, most common as I know
-        # self.geometry = self.dfs2.geometry
+        """Gets the Dfs2 file properties"""
+        self._dfs2 = Dfs2(self.path)
+        self._ncols = self._dfs2.nx
+        self._nrows = self._dfs2.ny
+        self._xllcorner = self._dfs2.origin[0] - (self._dfs2.dx / 2)
+        self._yllcorner = self._dfs2.origin[1] - (self._dfs2.dy / 2)
+        self._cellsize = self._dfs2.dx  # hope this value is same as dy
+        self._nodata_value = -9999
         return 1
 
     def write_asc(self):
+        "Creates an ASCII grid based on the Dfs2 input file"
         self.get_properties()
-        asc_path = self.path[:-4] + "asc"
-        ds = self.dfs2.read()[0]  # currently desiged to only read simple dfs2 files
+        ascpath = self.path[:-4] + "asc"
+        ds = self._dfs2.read()[0]  # currently desiged to only read simple dfs2 files
         da = mikeio.dataset.Dataset(ds)
         data = np.flip(
-            np.squeeze(np.nan_to_num(da.to_numpy(), nan=self.nodata_value)), 0
+            np.squeeze(np.nan_to_num(da.to_numpy(), nan=self._nodata_value)), 0
         )
-        with open(asc_path, "w") as file_obj:
-            file_obj.write("ncols " + str(self.ncols) + "\n")
-            file_obj.write("nrows " + str(self.nrows) + "\n")
-            file_obj.write("xllcorner " + str(self.xllcorner) + "\n")
-            file_obj.write("yllcorner " + str(self.yllcorner) + "\n")
-            file_obj.write("cellsize " + str(self.cellsize) + "\n")
-            file_obj.write("NODATA_value " + str(self.nodata_value) + "\n")
+        with open(ascpath, "w", encoding="utf-8") as file_obj:
+            file_obj.write("ncols " + str(self._ncols) + "\n")
+            file_obj.write("nrows " + str(self._nrows) + "\n")
+            file_obj.write("xllcorner " + str(self._xllcorner) + "\n")
+            file_obj.write("yllcorner " + str(self._yllcorner) + "\n")
+            file_obj.write("cellsize " + str(self._cellsize) + "\n")
+            file_obj.write("NODATA_value " + str(self._nodata_value) + "\n")
             for row in data:
                 for value in row:
                     file_obj.write(str(value) + " ")
--- code/functions.py
+++ code/functions.py
@@ -1,14 +1,20 @@
 import os
 
-# Basic functions
 
 def log_reader(folder, include_subdirectories):
+    """Read all log files
+
+    Args:
+        folder (str): Path to the folder where log file are saved
+        include_subdirectories (bool): defines if the subdirs should be checked
+    """
+
     def writer(path, f):
-        with open(os.path.join(path, f), 'r') as log:
+        with open(os.path.join(path, f), "r", encoding="utf-8") as log:
             lines = log.read().splitlines()
             status = lines[-1]
-            status = status.replace('=', '').lower().strip()
-            status = str(status) + '\t\t' + str(file.replace('.log', ''))
+            status = status.replace("=", "").lower().strip()
+            status = str(status) + "\t\t" + str(file.replace(".log", ""))
             status_list.append(status)
 
     # Write the results
@@ -18,37 +24,49 @@ def log_reader(folder, include_subdirectories):
     for root, dirs, files in os.walk(folder):
         for file in files:
             if include_subdirectories is False:
-                if file.endswith('.log') and root == folder:
+                if file.endswith(".log") and root == folder:
                     writer(root, file)
             else:
-                if file.endswith('.log'):
+                if file.endswith(".log"):
                     writer(root, file)
     status_list.sort()
     return status_list
 
 
 def mike_to_bat(folder):
+    """Create batch file to run multiple simulations
+
+    Args:
+        folder (str): Path to the folder where sim files are located
+
+    Returns:
+        str: all commands included in the .bat file
+    """
     commands_list = []
 
     # commands to be written. Not that this function will only work with Mike 2019
     for root, dirs, files in os.walk(folder):
         for file in files:
-            if file.endswith('.m21fm') and root == folder:
-                command = r'start /wait C:\"Program Files (x86)"\DHI\2019\bin\x64\MzLaunch.exe -gpu 1 -x ' \
-                          + str(file)
+            if file.endswith(".m21fm") and root == folder:
+                command = (
+                    r'start /wait C:\"Program Files (x86)"\DHI\2019\bin\x64\MzLaunch.exe -gpu 1 -x '
+                    + str(file)
+                )
                 commands_list.append(command)
-            if file.endswith('.m21') and root == folder:
-                command = r'start /wait C:\"Program Files (x86)"\DHI\2019\bin\x64\MzLaunch.exe -exit -mpi 6 ' \
-                          + str(file)
+            if file.endswith(".m21") and root == folder:
+                command = (
+                    r'start /wait C:\"Program Files (x86)"\DHI\2019\bin\x64\MzLaunch.exe -exit -mpi 6 '
+                    + str(file)
+                )
                 commands_list.append(command)
 
     # creating a txt file
     bat = folder + "\\Launcher.bat"
-    bat_writer = open(bat, "w+")
+    bat_writer = open(bat, "w+", encoding="utf-8")
 
     # saving commands to file
     for command in commands_list:
-        bat_writer.write('%s\r' % command)
+        bat_writer.write("%s\r" % command)
     bat_writer.close()
 
     return commands_list
