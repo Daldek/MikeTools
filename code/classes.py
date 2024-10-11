77f121c6b047ed639206d669bde754329c9e1732
PL1B5J <1b5j@sweco.dk>
2024-10-11 20:52:06 +0200

    Statistics, new dfs2 class and examples

--- code/classes.py
+++ code/classes.py
@@ -20,6 +20,11 @@ class AscFile:
         cellsize=None,
         nodata_value=None,
         data=None,
+        data_min=None,
+        data_mean=None,
+        data_max=None,
+        data_std=None,
+        data_variance=None,
     ):
         """Class initialization
         :param path: path to an ASCII (ESRI grid) file"""
@@ -31,6 +36,11 @@ class AscFile:
         self._cellsize = cellsize
         self._nodata_value = nodata_value
         self._data = data
+        self._data_min = data_min
+        self._data_mean = data_mean
+        self._data_max = data_max
+        self._data_std = data_std
+        self._data_variance = data_variance
 
     def _validate_file(self):
         "Validate if the input file exists and if it's an asc file"
@@ -97,7 +107,61 @@ class AscFile:
         data = np.array(self.get_data())
         reshaped_data = data.reshape((1, int(self._nrows), int(self._ncols)))
         reshaped_data[reshaped_data == self._nodata_value] = np.nan
-        return reshaped_data
+        self._data = reshaped_data
+        return self._data
+
+    def get_statistics(self):
+        """Calculates basic statistics of the input asc file
+
+        Returns:
+            float: min, mean, max and variance values
+        """
+        if all(
+            attr is None
+            for attr in [
+                self._data_min,
+                self._data_mean,
+                self._data_max,
+                self._data_std,
+                self._data_variance,
+            ]
+        ):
+            self._data_min = np.min(self._data)
+            self._data_mean = np.mean(self._data)
+            self._data_max = np.max(self._data)
+            self._data_std = np.std(self._data, mean=self._data_mean)
+            self._data_variance = np.var(self._data, mean=self._data_mean)
+        return (
+            self._data_min,
+            self._data_mean,
+            self._data_max,
+            self._data_std,
+            self._data_variance,
+        )
+
+    def create_dfs2(self, projection):
+        """Creates a Dfs2 file using properties from the ASCII file."""
+        self.get_properties()
+        grid = mikeio.Grid2D(
+            x0=self._xllcorner,
+            dx=self._cellsize,
+            y0=self._yllcorner,
+            dy=self._cellsize,
+            nx=int(self._ncols),
+            ny=int(self._nrows),
+            projection=projection,
+        )
+        da = mikeio.DataArray(
+            data=np.flip(self.get_data_as_3d_numpy_array(), 1),
+            geometry=grid,
+            time=pd.date_range("2100", periods=1, freq="D"),
+            item=mikeio.ItemInfo("Example", mikeio.EUMType.Elevation),
+        )
+
+        ds = mikeio.Dataset([da])
+        dfs2_name = self.path[:-3] + "dfs2"
+        ds.to_dfs(dfs2_name)
+        return dfs2_name
 
     @staticmethod
     def generate_random_grid(nrows=5, ncols=5, no_data_value=-9999):
@@ -112,47 +176,10 @@ class AscFile:
             f.write("CELLSIZE 1.0\n")
             f.write(f"NODATA_VALUE {no_data_value}\n")
             np.savetxt(f, data, fmt="%d")
-        return 1
+        return f
 
 
-class MyDfs2(AscFile):
-    """Class for creating a Dfs2 file based on an ASCII input file."""
-
-    def __init__(self, path, projection, item=None):
-        """Initializes the MyDfs2 class.
-        :param path: Path to the ASCII file.
-        :param projection: Coordinate system projection for the grid.
-        :param item: Item information for the Dfs2 file. NOT YET IMPLEMENTED!"""
-        super().__init__(path)
-        self.projection = projection
-        self.item = item
-
-    def create_dfs2(self):
-        """Creates a Dfs2 file using properties from the ASCII file."""
-        self.get_properties()
-        grid = mikeio.Grid2D(
-            x0=self._xllcorner,
-            dx=self._cellsize,
-            y0=self._yllcorner,
-            dy=self._cellsize,
-            nx=int(self._ncols),
-            ny=int(self._nrows),
-            projection=self.projection,
-        )
-        da = mikeio.DataArray(
-            data=np.flip(self.get_data_as_3d_numpy_array(), 1),
-            geometry=grid,
-            time=pd.date_range("2100", periods=1, freq="D"),
-            item=mikeio.ItemInfo("Example", mikeio.EUMType.Elevation),
-        )
-
-        ds = mikeio.Dataset([da])
-        dfs2_name = self.path[:-3] + "dfs2"
-        ds.to_dfs(dfs2_name)
-        return dfs2_name
-
-
-class MyASCII:
+class MyDfs2:
     """Class for creating an ASCII grid from a Dfs2 input file."""
 
     def __init__(self, path):
@@ -168,7 +195,7 @@ class MyASCII:
         self._nodata_value = None
 
     def _validate_file(self):
-        "Validate if the input file exists and if it's an asc file"
+        "Validate if the input file exists and if it's a dfs2 file"
         f_path = Path(self.path)
         if not f_path.exists():
             raise FileNotFoundError(f"{self.path} does not exists!")
@@ -177,25 +204,44 @@ class MyASCII:
 
     def get_properties(self):
         """Gets the Dfs2 file properties"""
-        self._dfs2 = Dfs2(self.path)
-        self._ncols = self._dfs2.nx
-        self._nrows = self._dfs2.ny
-        self._xllcorner = self._dfs2.origin[0] - (self._dfs2.dx / 2)
-        self._yllcorner = self._dfs2.origin[1] - (self._dfs2.dy / 2)
-        self._cellsize = self._dfs2.dx  # hope this value is same as dy
-        self._nodata_value = -9999
-        return 1
+        self._validate_file()
+        if all(
+            attr is None
+            for attr in [
+                self._ncols,
+                self._nrows,
+                self._xllcorner,
+                self._yllcorner,
+                self._cellsize,
+                self._nodata_value,
+            ]
+        ):
+            self._dfs2 = Dfs2(self.path)
+            self._ncols = self._dfs2.nx
+            self._nrows = self._dfs2.ny
+            self._xllcorner = self._dfs2.origin[0] - (self._dfs2.dx / 2)
+            self._yllcorner = self._dfs2.origin[1] - (self._dfs2.dy / 2)
+            self._cellsize = self._dfs2.dx  # hope this value is same as dy
+            self._nodata_value = -9999
+        return (
+            self._ncols,
+            self._nrows,
+            self._xllcorner,
+            self._yllcorner,
+            self._cellsize,
+            self._nodata_value,
+        )
 
     def write_asc(self):
-        "Creates an ASCII grid based on the Dfs2 input file"
+        "Creates an ASCII grid based on the dfs2 input file"
         self.get_properties()
-        ascpath = self.path[:-4] + "asc"
+        asc_path = self.path[:-4] + "asc"
         ds = self._dfs2.read()[0]  # currently desiged to only read simple dfs2 files
         da = mikeio.dataset.Dataset(ds)
         data = np.flip(
             np.squeeze(np.nan_to_num(da.to_numpy(), nan=self._nodata_value)), 0
         )
-        with open(ascpath, "w", encoding="utf-8") as file_obj:
+        with open(asc_path, "w", encoding="utf-8") as file_obj:
             file_obj.write("ncols " + str(self._ncols) + "\n")
             file_obj.write("nrows " + str(self._nrows) + "\n")
             file_obj.write("xllcorner " + str(self._xllcorner) + "\n")
